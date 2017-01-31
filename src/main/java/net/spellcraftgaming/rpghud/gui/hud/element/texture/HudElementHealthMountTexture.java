589dcc07be51cb84c6ab1695ce20c6c9624810a7
Akira Kuroda <kuroda.akira@yahoo.de>
2017-01-31 04:59:27 +0100

    Fixed Full Texture Health Elements

--- src/main/java/net/spellcraftgaming/rpghud/gui/hud/element/texture/HudElementHealthMountTexture.java
+++ src/main/java/net/spellcraftgaming/rpghud/gui/hud/element/texture/HudElementHealthMountTexture.java
@@ -1,6 +1,7 @@
 package net.spellcraftgaming.rpghud.gui.hud.element.texture;
 
 import net.minecraft.client.gui.Gui;
+import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.entity.EntityLivingBase;
 import net.spellcraftgaming.rpghud.gui.hud.element.HudElementTexture;
 import net.spellcraftgaming.rpghud.gui.hud.element.HudElementType;
@@ -23,6 +24,13 @@ public class HudElementHealthMountTexture extends HudElementTexture{
 		int health = (int) Math.ceil(mount.getHealth());
 		int healthMax = (int) mount.getMaxHealth();
 		gui.drawTexturedModalRect(53, 54, 0, 124, (int) (88.0D * ((double) health / (double) healthMax)), 8);
+		
+		String stringHealth = health + "/" + healthMax;
+		if(this.settings.show_numbers_health) {
+			GlStateManager.scale(0.5, 0.5, 0.5);
+			gui.drawCenteredString(this.mc.fontRendererObj, stringHealth, 194, 112, -1);
+			GlStateManager.scale(2.0, 2.0, 2.0);
+		}
 		bind(Gui.ICONS);
 	}
 
--- src/main/java/net/spellcraftgaming/rpghud/gui/hud/element/texture/HudElementHealthTexture.java
+++ src/main/java/net/spellcraftgaming/rpghud/gui/hud/element/texture/HudElementHealthTexture.java
@@ -3,6 +3,7 @@ package net.spellcraftgaming.rpghud.gui.hud.element.texture;
 import net.minecraft.client.gui.Gui;
 import net.minecraft.entity.SharedMonsterAttributes;
 import net.minecraft.entity.ai.attributes.IAttributeInstance;
+import net.minecraft.init.MobEffects;
 import net.minecraft.util.math.MathHelper;
 import net.spellcraftgaming.rpghud.gui.hud.element.HudElementTexture;
 import net.spellcraftgaming.rpghud.gui.hud.element.HudElementType;
@@ -24,7 +25,14 @@ public class HudElementHealthTexture extends HudElementTexture{
 		int health = MathHelper.ceil(this.mc.player.getHealth());
 		IAttributeInstance attrMaxHealth = this.mc.player.getEntityAttribute(SharedMonsterAttributes.MAX_HEALTH);
 		int maxHealth = (int) attrMaxHealth.getAttributeValue();
-		gui.drawTexturedModalRect(49, 9, 0, 100, (int) (110.0D * ((double) health / (double) maxHealth)), 12);
+		if (this.mc.player.isPotionActive(MobEffects.POISON)) {
+			gui.drawTexturedModalRect(49, 9, 141, 160, (int) (110.0D * ((double) health / (double) maxHealth)), 12);
+		} else {
+			gui.drawTexturedModalRect(49, 9, 0, 100, (int) (110.0D * ((double) health / (double) maxHealth)), 12);
+		}
+	
+		String stringHealth = health + "/" + maxHealth;
+		if(this.settings.show_numbers_health) gui.drawCenteredString(this.mc.fontRendererObj, stringHealth, 49 + 55, 11, -1);
 		bind(Gui.ICONS);
 	}
 
