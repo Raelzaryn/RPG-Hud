65092c1f3c1493da1cdf67ab71a99547d7897c71
Akira Kuroda <kuroda.akira@yahoo.de>
2017-03-09 15:58:11 +0100

    Added absorption support

--- src/main/java/net/spellcraftgaming/rpghud/gui/hud/element/defaulthud/HudElementHealthDefault.java
+++ src/main/java/net/spellcraftgaming/rpghud/gui/hud/element/defaulthud/HudElementHealthDefault.java
@@ -23,15 +23,20 @@ public class HudElementHealthDefault extends HudElementBarred {
 	public void drawElement(Gui gui, float zLevel, float partialTicks) {
 		int health = MathHelper.ceiling_float_int(this.mc.thePlayer.getHealth());
 		IAttributeInstance attrMaxHealth = this.mc.thePlayer.getEntityAttribute(SharedMonsterAttributes.MAX_HEALTH);
-		int maxHealth = (int) attrMaxHealth.getAttributeValue();
+		int absorption = MathHelper.floor_float(this.mc.thePlayer.getAbsorptionAmount());
+		int healthMax = (int) attrMaxHealth.getAttributeValue();
 		int posX = this.settings.render_player_face ? 49 : 24;
 		int posY = this.settings.render_player_face ? 13 : 5;
+		
+		if(absorption > 1) drawCustomBar(posX, posY, 110, 12, (double) (health + absorption)/ (double) (healthMax + absorption) * 100D, -1, -1, this.settings.color_absorption, offsetColorPercent(this.settings.color_absorption, OFFSET_PERCENT));
+		
 		if (this.mc.thePlayer.isPotionActive(MobEffects.POISON)) {
-			drawCustomBar(posX, posY, 110, 12, (double) health / (double) maxHealth * 100D, -1, -1, this.settings.color_poison, offsetColorPercent(this.settings.color_poison, OFFSET_PERCENT));
+			drawCustomBar(posX, posY, 110, 12, (double) health / (double) (healthMax + absorption) * 100D, -1, -1, this.settings.color_poison, offsetColorPercent(this.settings.color_poison, OFFSET_PERCENT));
 		} else {
-			drawCustomBar(posX, posY, 110, 12, (double) health / (double) maxHealth * 100D, -1, -1, this.settings.color_health, offsetColorPercent(this.settings.color_health, OFFSET_PERCENT));
+			drawCustomBar(posX, posY, 110, 12, (double) health / (double) (healthMax + absorption) * 100D, -1, -1, this.settings.color_health, offsetColorPercent(this.settings.color_health, OFFSET_PERCENT));
 		}
-		String stringHealth = health + "/" + maxHealth;
+		
+		String stringHealth = (health + absorption) + "/" + healthMax;
 		if (this.settings.show_numbers_health)
 			gui.drawCenteredString(this.mc.fontRendererObj, stringHealth, posX + 55, posY + 2, -1);
 	}
