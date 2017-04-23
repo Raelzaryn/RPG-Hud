0696892f6ee378cc7089cc67cc3a2c35cabe8296
Akira Kuroda <kuroda.akira@yahoo.de>
2017-04-23 13:55:02 +0200

    fixed crash when holding food in offhand and no item in mainhand

--- src/main/java/net/spellcraftgaming/rpghud/gui/hud/element/defaulthud/HudElementFoodDefault.java
+++ src/main/java/net/spellcraftgaming/rpghud/gui/hud/element/defaulthud/HudElementFoodDefault.java
@@ -17,7 +17,7 @@ public class HudElementFoodDefault extends HudElementBarred {
 	public boolean checkConditions() {
 		return GameData.shouldDrawHUD();
 	}
-	
+
 	@Override
 	public void drawElement(Gui gui, float zLevel, float partialTicks) {
 		int stamina = GameData.getPlayerFood();
@@ -26,21 +26,27 @@ public class HudElementFoodDefault extends HudElementBarred {
 		int posY = this.settings.render_player_face ? 26 : 18;
 		ItemStack itemMain = GameData.getMainhand();
 		ItemStack itemSec = GameData.getOffhand();
-		if ((itemMain != GameData.nullStack() && itemMain.getItem() instanceof ItemFood) || (itemSec != GameData.nullStack() && itemSec.getItem() instanceof ItemFood) && GameData.doesPlayerNeedFood() && this.settings.show_hunger_preview) {
-			float value;
-			if(itemMain.getItem() instanceof ItemFood) 
+
+		if (GameData.doesPlayerNeedFood() && this.settings.show_hunger_preview) {
+			float value = 0;
+			if (itemMain != GameData.nullStack() && itemMain.getItem() instanceof ItemFood) {
 				value = ((ItemFood) itemMain.getItem()).getHealAmount(itemMain);
-			else value = ((ItemFood) itemSec.getItem()).getHealAmount(itemSec);
-			int bonusHunger = (int) (value + stamina);
-			if (bonusHunger > foodMax)
-				bonusHunger = foodMax;
-			int colorPreview = offsetColor(this.settings.color_stamina, OFFSET_PREVIEW);
-			drawCustomBar(posX, posY, 110, 12, bonusHunger / (double)foodMax * 100.0D, -1, -1, colorPreview, offsetColorPercent(colorPreview, OFFSET_PERCENT));
+			} else if (itemSec != GameData.nullStack() && itemSec.getItem() instanceof ItemFood) {
+				value = ((ItemFood) itemSec.getItem()).getHealAmount(itemSec);
+			}
+			if (value > 0) {
+				int bonusHunger = (int) (value + stamina);
+				if (bonusHunger > foodMax)
+					bonusHunger = foodMax;
+				int colorPreview = offsetColor(this.settings.color_stamina, OFFSET_PREVIEW);
+				drawCustomBar(posX, posY, 110, 12, bonusHunger / (double) foodMax * 100.0D, -1, -1, colorPreview, offsetColorPercent(colorPreview, OFFSET_PERCENT));
+			}
 		}
+
 		if (GameData.isPlayerHungered()) {
-			drawCustomBar(posX, posY, 110, 12, stamina / (double)foodMax * 100.0D, -1, -1, this.settings.color_hunger, offsetColorPercent(this.settings.color_hunger, OFFSET_PERCENT));
+			drawCustomBar(posX, posY, 110, 12, stamina / (double) foodMax * 100.0D, -1, -1, this.settings.color_hunger, offsetColorPercent(this.settings.color_hunger, OFFSET_PERCENT));
 		} else {
-			drawCustomBar(posX, posY, 110, 12, stamina / (double)foodMax * 100.0D, -1, -1, this.settings.color_stamina, offsetColorPercent(this.settings.color_stamina, OFFSET_PERCENT));
+			drawCustomBar(posX, posY, 110, 12, stamina / (double) foodMax * 100.0D, -1, -1, this.settings.color_stamina, offsetColorPercent(this.settings.color_stamina, OFFSET_PERCENT));
 		}
 		String staminaString = stamina + "/" + foodMax;
 		if (this.settings.show_numbers_stamina)
--- src/main/java/net/spellcraftgaming/rpghud/gui/hud/element/modern/HudElementFoodModern.java
+++ src/main/java/net/spellcraftgaming/rpghud/gui/hud/element/modern/HudElementFoodModern.java
@@ -10,7 +10,7 @@ import net.spellcraftgaming.rpghud.gui.hud.HudModern;
 import net.spellcraftgaming.rpghud.gui.hud.element.HudElementBarred;
 import net.spellcraftgaming.rpghud.gui.hud.element.HudElementType;
 
-public class HudElementFoodModern extends HudElementBarred{
+public class HudElementFoodModern extends HudElementBarred {
 
 	public HudElementFoodModern() {
 		super(HudElementType.FOOD, 0, 0, 0, 0, true);
@@ -20,44 +20,49 @@ public class HudElementFoodModern extends HudElementBarred{
 	public boolean checkConditions() {
 		return GameData.shouldDrawHUD();
 	}
-	
+
 	@Override
 	public void drawElement(Gui gui, float zLevel, float partialTicks) {
 		int stamina = GameData.getPlayerFood();
 		int staminaMax = GameData.getPlayerMaxFood();
 		int xOffset = ((HudModern) this.rpgHud.huds.get("modern")).getPosX();
-		
-		int width = this.mc.fontRendererObj.getStringWidth(staminaMax + "/"  + staminaMax) / 2 + 4;
-		
+
+		int width = this.mc.fontRendererObj.getStringWidth(staminaMax + "/" + staminaMax) / 2 + 4;
+
 		String staminaString = stamina + "/" + staminaMax;
-		if(this.settings.show_numbers_health && this.settings.show_numbers_stamina){
+		if (this.settings.show_numbers_health && this.settings.show_numbers_stamina) {
 			drawRect(this.settings.render_player_face ? 23 : 2, 12, width, 8, 0xA0000000);
 			GL11.glScaled(0.5D, 0.5D, 0.5D);
-			gui.drawCenteredString(this.mc.fontRendererObj, staminaString, this.settings.render_player_face ? (xOffset * 2) + 28: 24, 28, -1);
+			gui.drawCenteredString(this.mc.fontRendererObj, staminaString, this.settings.render_player_face ? (xOffset * 2) + 28 : 24, 28, -1);
 			GL11.glScaled(2.0D, 2.0D, 2.0D);
 		}
-		int posX = (this.settings.render_player_face ? 24 : 2) + ((this.settings.show_numbers_health && this.settings.show_numbers_stamina) ? xOffset + 1: 0);
-		
+		int posX = (this.settings.render_player_face ? 24 : 2) + ((this.settings.show_numbers_health && this.settings.show_numbers_stamina) ? xOffset + 1 : 0);
+
 		drawTetragon(posX, posX, 13, 13, 70, 58, 8, 8, 0xA0000000);
 		drawTetragon(posX + 2, posX + 2, 13, 13, 64, 54, 6, 6, 0x20FFFFFF);
-		
+
 		ItemStack itemMain = GameData.getMainhand();
 		ItemStack itemSec = GameData.getOffhand();
-		
-		if ((itemMain != GameData.nullStack() && itemMain.getItem() instanceof ItemFood) || (itemSec != GameData.nullStack() && itemSec.getItem() instanceof ItemFood) && GameData.doesPlayerNeedFood() && this.settings.show_hunger_preview) {
-			float value;
-			if(itemMain.getItem() instanceof ItemFood) 
+
+		if (GameData.doesPlayerNeedFood() && this.settings.show_hunger_preview) {
+			float value = 0;
+			if (itemMain != GameData.nullStack() && itemMain.getItem() instanceof ItemFood) {
 				value = ((ItemFood) itemMain.getItem()).getHealAmount(itemMain);
-			else value = ((ItemFood) itemSec.getItem()).getHealAmount(itemSec);
-			int bonusHunger = (int) (value + stamina);
-			if (bonusHunger > 20)
-				bonusHunger = 20;
-			drawTetragon(posX + 2, posX + 2, 13, 13, (int) (64 * ((double)bonusHunger / (double) staminaMax)), (int) (63 * ((double)bonusHunger / (double) 20)) - 10, 6, 6, offsetColor(this.settings.color_stamina, OFFSET_PREVIEW));
+			} else if (itemSec != GameData.nullStack() && itemSec.getItem() instanceof ItemFood) {
+				value = ((ItemFood) itemSec.getItem()).getHealAmount(itemSec);
+			}
+			if (value > 0) {
+				int bonusHunger = (int) (value + stamina);
+				if (bonusHunger > staminaMax)
+					bonusHunger = staminaMax;
+				drawTetragon(posX + 2, posX + 2, 13, 13, (int) (64 * ((double) bonusHunger / (double) staminaMax)), (int) (63 * ((double) bonusHunger / (double) 20)) - 10, 6, 6, offsetColor(this.settings.color_stamina, OFFSET_PREVIEW));
+			}
 		}
+
 		if (GameData.isPlayerHungered()) {
-			drawTetragon(posX + 2, posX + 2, 13, 13, (int) (64 * ((double)stamina / (double) staminaMax)), (int) (64 * ((double)stamina / (double) 20)) - 10, 6, 6, this.settings.color_hunger);
+			drawTetragon(posX + 2, posX + 2, 13, 13, (int) (64 * ((double) stamina / (double) staminaMax)), (int) (64 * ((double) stamina / (double) 20)) - 10, 6, 6, this.settings.color_hunger);
 		} else {
-			drawTetragon(posX + 2, posX + 2, 13, 13, (int) (64 * ((double)stamina / (double) staminaMax)), (int) (64 * ((double)stamina / (double) 20)) - 10, 6, 6, this.settings.color_stamina);
+			drawTetragon(posX + 2, posX + 2, 13, 13, (int) (64 * ((double) stamina / (double) staminaMax)), (int) (64 * ((double) stamina / (double) 20)) - 10, 6, 6, this.settings.color_stamina);
 		}
 	}
 
