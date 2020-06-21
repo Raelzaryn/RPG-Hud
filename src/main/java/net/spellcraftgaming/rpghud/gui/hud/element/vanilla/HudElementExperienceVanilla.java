21b25331d5b49cf3e57f22d03397cdfc9e7043b5
KurodaAkira <kuroda.akira@yahoo.de>
2020-06-21 12:22:43 +0200

    minor fixes

--- src/main/java/net/spellcraftgaming/rpghud/gui/hud/element/vanilla/HudElementExperienceVanilla.java
+++ src/main/java/net/spellcraftgaming/rpghud/gui/hud/element/vanilla/HudElementExperienceVanilla.java
@@ -3,6 +3,7 @@ package net.spellcraftgaming.rpghud.gui.hud.element.vanilla;
 import com.mojang.blaze3d.platform.GlStateManager;
 
 import net.minecraft.client.gui.AbstractGui;
+import net.minecraft.entity.LivingEntity;
 import net.spellcraftgaming.rpghud.gui.hud.element.HudElement;
 import net.spellcraftgaming.rpghud.gui.hud.element.HudElementType;
 
@@ -14,7 +15,7 @@ public class HudElementExperienceVanilla extends HudElement {
 
 	@Override
 	public boolean checkConditions() {
-		return !(this.mc.player.getRidingEntity().isLiving()) && !this.mc.gameSettings.hideGUI && this.mc.playerController.shouldDrawHUD();
+		return !(this.mc.player.getRidingEntity() instanceof LivingEntity) && !this.mc.gameSettings.hideGUI && this.mc.playerController.shouldDrawHUD();
 	}
 
 	@Override
