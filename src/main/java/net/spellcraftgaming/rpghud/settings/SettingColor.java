e2c1fc415d3d3316024bb8810a674854212159ba
KurodaAkira <kuroda.akira@yahoo.de>
2020-07-27 00:12:14 +0200

    added status effects element and scaling option for it

--- src/main/java/net/spellcraftgaming/rpghud/settings/SettingColor.java
+++ src/main/java/net/spellcraftgaming/rpghud/settings/SettingColor.java
@@ -1,53 +1,57 @@
 package net.spellcraftgaming.rpghud.settings;
 
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 import net.spellcraftgaming.rpghud.gui.hud.element.HudElementType;
 
-public class SettingColor extends Setting {
+@SideOnly(Side.CLIENT)
+public class SettingColor extends Setting{
 
-    public final int defaultColor;
-    public int color;
+	public final int defaultColor;
+	public int color;
+	
+	public SettingColor(String ID, int color) {
+		super(ID);
+		this.defaultColor = color;
+		this.color = color;
+	}
+	
+	public SettingColor(String ID, HudElementType type, int color) {
+		super(ID, type);
+		this.defaultColor = color;
+		this.color = color;
+	}
 
-    public SettingColor(String ID, int color) {
-        super(ID);
-        this.defaultColor = color;
-        this.color = color;
-    }
+	@Override
+	public void increment() {
+	}
+	
+	public void setColor(int color){
+		this.color = color;
+	}
 
-    public SettingColor(String ID, HudElementType type, int color) {
-        super(ID, type);
-        this.defaultColor = color;
-        this.color = color;
-    }
+	@Override
+	public Object getValue() {
+		return this.color;
+	}
 
-    @Override
-    public void increment() {
-    }
+	@Override
+	public void resetValue() {
+		this.color = this.defaultColor;
+	}
 
-    public void setColor(int color) {
-        this.color = color;
-    }
+	@Override
+	public Setting setValue(Object o) {
+		if (o instanceof String) {
+			this.color = Integer.parseInt((String) o, 16);
+		} else if(o instanceof Integer) {
+			this.color = (Integer) o;
+		}
+		return this;
+	}
 
-    @Override
-    public Object getValue() {
-        return this.color;
-    }
-
-    @Override
-    public void resetValue() {
-        this.color = this.defaultColor;
-    }
-
-    @Override
-    public void setValue(Object o) {
-        if(o instanceof String) {
-            this.color = Integer.parseInt((String) o, 16);
-        } else if(o instanceof Integer) {
-            this.color = (Integer) o;
-        }
-    }
-
-    @Override
-    public Object getDefaultValue() {
-        return this.defaultColor;
-    }
+	@Override
+	public Object getDefaultValue() {
+		return this.defaultColor;
+	}
 }
