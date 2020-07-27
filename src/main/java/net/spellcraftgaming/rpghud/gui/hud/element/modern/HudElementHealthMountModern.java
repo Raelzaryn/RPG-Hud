062c62714dce9f2c0895fbed97b31994b593980e
KurodaAkira <kuroda.akira@yahoo.de>
2020-07-27 11:45:00 +0200

    fixed mount and entity inspect health render

--- src/main/java/net/spellcraftgaming/rpghud/gui/hud/element/modern/HudElementHealthMountModern.java
+++ src/main/java/net/spellcraftgaming/rpghud/gui/hud/element/modern/HudElementHealthMountModern.java
@@ -16,38 +16,42 @@ import net.spellcraftgaming.rpghud.settings.Settings;
 @OnlyIn(Dist.CLIENT)
 public class HudElementHealthMountModern extends HudElement {
 
-	public HudElementHealthMountModern() {
-		super(HudElementType.HEALTH_MOUNT, 0, 0, 0, 0, false);
-		parent = HudElementType.WIDGET;
-	}
+    public HudElementHealthMountModern() {
+        super(HudElementType.HEALTH_MOUNT, 0, 0, 0, 0, false);
+        parent = HudElementType.WIDGET;
+    }
 
-	@Override
-	public boolean checkConditions() {
-		return this.mc.player.getRidingEntity() instanceof LivingEntity && this.mc.playerController.shouldDrawHUD();
-	}
+    @Override
+    public boolean checkConditions() {
+        return this.mc.player.getRidingEntity() instanceof LivingEntity && this.mc.playerController.shouldDrawHUD();
+    }
 
-	@Override
-	public void drawElement(AbstractGui gui, MatrixStack ms, float zLevel, float partialTicks, int scaledWidth, int scaledHeight) {
-		LivingEntity mount = (LivingEntity) this.mc.player.getRidingEntity();
-		int health = (int) Math.ceil(mount.getHealth());
-		int healthMax = (int) mount.getMaxHealth();
-		int xOffset = ((HudModern) this.rpgHud.huds.get("modern")).getPosX();
-		
-		String stringHealth = this.settings.getBoolValue(Settings.mount_health_percentage) ? (int) Math.floor((double) health / (double) healthMax * 100) + "%" : health + "/" + healthMax;
+    @Override
+    public void drawElement(AbstractGui gui, MatrixStack ms, float zLevel, float partialTicks, int scaledWidth, int scaledHeight) {
+        LivingEntity mount = (LivingEntity) this.mc.player.getRidingEntity();
+        int health = (int) Math.ceil(mount.getHealth());
+        int healthMax = (int) mount.getMaxHealth();
+        if(health > healthMax) health = healthMax;
+        int xOffset = ((HudModern) this.rpgHud.huds.get("modern")).getPosX();
 
-		int posX = (this.settings.getBoolValue(Settings.render_player_face) ? 26 : 4) + (this.settings.getBoolValue(Settings.show_numbers_health) ? xOffset -2 : -2) + this.settings.getPositionValue(Settings.mount_health_position)[0];
-		int posY = this.settings.getPositionValue(Settings.mount_health_position)[1];
-		if (this.settings.getBoolValue(Settings.show_numbers_health)) {
-			int width2 = this.mc.fontRenderer.getStringWidth(stringHealth) / 2;
-			drawRect(posX, 24 + posY, width2 + 4, 5, 0xA0000000);
-			GL11.glScaled(0.5D, 0.5D, 0.5D);
-			gui.func_238476_c_(ms, this.mc.fontRenderer, stringHealth, posX * 2 + 4, 48 + posY * 2, -1);
-			GL11.glScaled(2.0D, 2.0D, 2.0D);
-		}
+        String stringHealth = this.settings.getBoolValue(Settings.mount_health_percentage) ? (int) Math.floor((double) health / (double) healthMax * 100) + "%"
+                : health + "/" + healthMax;
 
-		drawTetragon(posX, posX, 21 + posY, 21 + posY, 58, 54, 3, 3, 0xA0000000);
-		drawTetragon(posX + 2, posX + 2, 21 + posY, 21 + posY, (int) (((double) health / (double) healthMax) * 53), (int) (((double) health / (double) healthMax) * 53 - 2), 1, 1, this.settings.getIntValue(Settings.color_health));
+        int posX = (this.settings.getBoolValue(Settings.render_player_face) ? 26 : 4) + (this.settings.getBoolValue(Settings.show_numbers_health) ? xOffset - 2 : -2)
+                + this.settings.getPositionValue(Settings.mount_health_position)[0];
+        int posY = this.settings.getPositionValue(Settings.mount_health_position)[1];
+        if(this.settings.getBoolValue(Settings.show_numbers_health)) {
+            int width2 = this.mc.fontRenderer.getStringWidth(stringHealth) / 2;
+            drawRect(posX, 24 + posY, width2 + 4, 5, 0xA0000000);
+            GL11.glScaled(0.5D, 0.5D, 0.5D);
+            gui.func_238476_c_(ms, this.mc.fontRenderer, stringHealth, posX * 2 + 4, 48 + posY * 2, -1);
+            GL11.glScaled(2.0D, 2.0D, 2.0D);
+        }
 
-	}
+        drawTetragon(posX, posX, 21 + posY, 21 + posY, 58, 54, 3, 3, 0xA0000000);
+        drawTetragon(posX + 2, posX + 2, 21 + posY, 21 + posY, (int) (((double) health / (double) healthMax) * 53),
+                (int) (((double) health / (double) healthMax) * 53 - 2), 1, 1, this.settings.getIntValue(Settings.color_health));
+
+    }
 
 }
