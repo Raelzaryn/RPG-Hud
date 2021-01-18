bff00078ef9a15fd5076248c86db0bef5488532f
KurodaAkira <kuroda.akira@yahoo.de>
2021-01-18 18:50:28 +0100

    Fixed config file issues

--- src/main/java/net/spellcraftgaming/rpghud/settings/Settings.java
+++ src/main/java/net/spellcraftgaming/rpghud/settings/Settings.java
@@ -113,7 +113,7 @@ public class Settings {
 
     private File rpgHudDir() {
         Minecraft mc = Minecraft.getInstance();
-        return (new File(mc.gameDir.getPath(), "config" + File.separator + "RPG-HUD"));
+        return (new File(mc.gameDir.getPath(), "config" + File.separator + "RPG-HUD.cfg"));
     }
 
     public Settings() {
@@ -384,6 +384,13 @@ public class Settings {
             }
 
             if(!file.exists()) {
+                Minecraft mc = Minecraft.getInstance();
+                File oldfile = new File(mc.gameDir.getPath(), "config" + File.separator + "RPG-HUD");
+                if(oldfile.exists() && !oldfile.isDirectory()) {
+                    System.out.println("Converted old config file");
+                    oldfile.renameTo(file);
+                    load();
+                }
                 // Either a previous load attempt failed or the file is new; clear maps
                 if(!file.createNewFile())
                     return;
