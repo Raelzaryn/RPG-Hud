95dcb68251bddf85e07c94a94aaba880bf5bf82a
KurodaAkira <kuroda.akira@spellcraftgaming.net>
2022-05-28 17:16:40 +0200

    Fixed simple hud class path

--- src/main/java/net/spellcraftgaming/rpghud/gui/hud/element/simple/HudElementArmorSimple.java
+++ src/main/java/net/spellcraftgaming/rpghud/gui/hud/element/simple/HudElementArmorSimple.java
@@ -1,4 +1,4 @@
-package net.spellcraftgaming.rpghud.gui.hud.simple;
+package net.spellcraftgaming.rpghud.gui.hud.element.simple;
 
 import static net.minecraft.client.gui.GuiComponent.GUI_ICONS_LOCATION;
 
